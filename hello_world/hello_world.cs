09e22a9936522cf6353677f04b580c901e3b51d9
eseegm97 <eseegm97@gmail.com>
2026-01-14 20:24:46 -0600

    hello_world

---
+++ hello_world/hello_world.cs
@@ -0,0 +1,9 @@
+// Console.WriteLine("Hello World!");
+
+// Console.WriteLine("Congratulations!");
+// Console.Write("You wrote your first line of code.");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
