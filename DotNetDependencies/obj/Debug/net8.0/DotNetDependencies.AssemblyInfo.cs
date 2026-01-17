33c8fca8c514b8042f118e113c409db71e5e3bbf
eseegm97 <eseegm97@gmail.com>
2026-01-16 20:27:07 -0600

    changes

--- DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4bac9dfe34bc627a5ce01a5770d2e4ad02bd26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d6b298085c7cc14928ce1fe90b0a4c7bcfe660d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
