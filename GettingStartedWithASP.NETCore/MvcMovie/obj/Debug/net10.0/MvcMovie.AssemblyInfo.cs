53fd79ee699eefe7876d8a75518fdb0a5c690509
eseegm97 <eseegm97@gmail.com>
2026-01-27 10:55:41 -0600

    ASP.NET_core

--- GettingStartedWithASP.NETCore/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ GettingStartedWithASP.NETCore/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766188118e985ff4ced20ae75944eefee450ec50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1c56b5318583472d6b621024da71dced64c001")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
